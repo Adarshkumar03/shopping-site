a09370bb1816ef4e2b4223cf0ed9c546721f15ac
Adarsh Kumar <54439573+Adarshkumar03@users.noreply.github.com>
2021-10-17 10:01:45 +0530

    Update Features.js

--- src/Features.js
+++ src/Features.js
@@ -3,7 +3,7 @@ import Card from "./Card";
 const Features = () => {
   return (
     <div>
-      /
+      <h2>New Products</h2>
       <Card img="#" name="Some name"/>
       <Card img="#" name="Some name"/>
       <Card img="#" name="Some name"/>
