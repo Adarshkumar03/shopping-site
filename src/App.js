02e63c8032ecd7e00c02006e4bc307529b19cf92
Adarsh Kumar <54439573+Adarshkumar03@users.noreply.github.com>
2021-10-17 10:14:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import Features from "./Features";
-import Comment from "./Comment";
+import Comments from "./Comments";
+import Cart from "./Cart";
 
 function App() {
   return (
     <div className="App">
-      <h1>This is a App component</h1>
+      <Cart />
+      <Header />
+      <Features />
+      <Comments />
+      <Footer />
     </div>
   );
 }
